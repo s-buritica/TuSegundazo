3df4cb649aeba97290b2528a2726e23c892060eb
Simón Buriticá <s.buritica@uniandes.edu.co>
2023-11-05 09:38:11 -0500

    Creación Modelo Car

--- src/app/car/car-list/car-list.component.ts
+++ src/app/car/car-list/car-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../car';
 
 @Component({
   selector: 'app-car-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
 
+  cars: Array<Car> = [];
   constructor() { }
 
   ngOnInit() {
